3fc05aace8a2dd8e42da59952a854e1f623414e2
Manish Bulchandani <manishbulchandani@Manishs-MacBook-Air.local>
2026-03-22 15:49:58 +0530

    build error resoltion

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useState, useEffect, useMemo, useCallback } from 'react';
+import { StrictMode, useState, useMemo, useCallback } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
